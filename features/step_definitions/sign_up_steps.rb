d66847767f856140f218cc08d134c46ec711171b
Serafeim Maroulis <seramak@gmail.com>
2014-07-01 20:00:27 +0100

    Created Registration page

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -6,11 +6,13 @@ Given(/^that the user is on the registration page$/) do
 end
 
 When(/^the user enters his details$/) do
-   fill_in("First name", :with => Faker::Name.first_name)
-   fill_in("Last name", :with => Faker::Name.last_name)
-   fill_in("Email", :with => Faker::Internet.email )
-   fill_in("Password", :with => "password")
-   fill_in("Password confirmation", :with => "password")
+  within all('.registration').last do
+    fill_in("First name", :with => Faker::Name.first_name)
+    fill_in("Last name", :with => Faker::Name.last_name)
+    fill_in("Email", :with => Faker::Internet.email )
+    fill_in("Password", :with => "password")
+    fill_in("Password confirmation", :with => "password")
+  end
 end
 
 When(/^the user selects a type of account$/) do
@@ -22,6 +24,5 @@ When(/^the user clicks the submit button$/) do
 end
 
 Then(/^a new account should be created$/) do
-  binding.pry
   Seller.all.count.should eq(1)
 end
